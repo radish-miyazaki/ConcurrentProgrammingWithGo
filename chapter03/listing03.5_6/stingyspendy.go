9efeafad41bf598223f7aa794ee6bd34db26cf14
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2025-02-14 13:33:59 +0900

    finished chapter 3

---
+++ chapter03/listing03.5_6/stingyspendy.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func stingy(money *int) {
+	for i := 0; i < 1_000_000; i++ {
+		*money += 10
+	}
+	fmt.Println("Stingy Done")
+}
+
+func spendy(money *int) {
+	for i := 0; i < 1_000_000; i++ {
+		*money -= 10
+	}
+	fmt.Println("Spendy Done")
+}
+
+func main() {
+	money := 100
+	go stingy(&money)
+	go spendy(&money)
+
+	time.Sleep(2 * time.Second)
+	println("Money in bank account: ", money)
+}
