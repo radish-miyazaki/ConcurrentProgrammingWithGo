6cc2d5c7f80a8eaae48ae9172871e86fad51d1ca
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2025-02-28 23:33:19 +0900

    finished chapter 9

--- chapter09/listing09.18_19/wordstatsearlyquit.go
+++ chapter09/listing09.18_19/wordstatsearlyquit.go
@@ -13,20 +13,48 @@ import (
 const downloaders = 20
 
 func main() {
+	quitWords := make(chan struct{})
 	quit := make(chan struct{})
 	defer close(quit)
 
-	urls := generateURLs(quit)
+	urls := generateURLs(quitWords)
 	pages := make([]<-chan string, downloaders)
 	for i := range downloaders {
-		pages[i] = downloadedPages(quit, urls)
+		pages[i] = downloadedPages(quitWords, urls)
 	}
-	words := extractWords(quit, FanIn(quit, pages))
+	words := Take(quitWords, 10_000, extractWords(quit, FanIn(quitWords, pages)))
 	wordsMulti := Broadcast(quit, words, 2)
 	longestResults := longestWords(quit, wordsMulti[0])
 	frequentResults := frequentWords(quit, wordsMulti[1])
 	fmt.Println("Longest words:", <-longestResults)
 	fmt.Println("Frequent words:", <-frequentResults)
+}
+
+func Take[K any](quit chan struct{}, n int, input <-chan K) <-chan K {
+	output := make(chan K)
+	go func() {
+		defer close(output)
+
+		var msg K
+		moreData := true
+		for n > 0 && moreData {
+			select {
+			case msg, moreData = <-input:
+				if moreData {
+					output <- msg
+					n--
+				}
+			case <-quit:
+				return
+			}
+		}
+
+		if n == 0 {
+			close(quit)
+		}
+	}()
+
+	return output
 }
 
 func Broadcast[K any](quit <-chan struct{}, input <-chan K, n int) []chan K {
