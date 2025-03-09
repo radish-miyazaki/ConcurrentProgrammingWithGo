8297993af5b00b7e3b47d65b02d0654719d60a5c
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2025-03-09 17:05:35 +0900

    solve all chapter 11 exercises

--- exercises/chapter11/exercise11.2/allfilesinfoselect.go
+++ exercises/chapter11/exercise11.2/allfilesinfoselect.go
@@ -1,3 +1,78 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	filesCh := make(chan string)
+	dirsCh := make(chan string)
+
+	go handleFiles(filesCh, dirsCh)
+	go handleDirectories(dirsCh, filesCh)
+
+	dirsCh <- os.Args[1]
+
+	time.Sleep(60 * time.Second)
+}
+
+func handleDirectories(dirs <-chan string, files chan<- string) {
+	toPush := make([]string, 0)
+	appendAllFiles := func(path string) {
+		fmt.Println("Reading all files from", path)
+		filesInDir, _ := os.ReadDir(path)
+		fmt.Printf("Pushing %d files from %s\n", len(filesInDir), path)
+
+		for _, file := range filesInDir {
+			toPush = append(toPush, filepath.Join(path, file.Name()))
+		}
+	}
+
+	for {
+		if len(toPush) == 0 {
+			appendAllFiles(<-dirs)
+			continue
+		}
+
+		select {
+		case fullpath := <-dirs:
+			appendAllFiles(fullpath)
+		case files <- toPush[0]:
+			toPush = toPush[1:]
+		}
+	}
+}
+
+func handleFiles(files <-chan string, dirs chan<- string) {
+	dirsToPush := make([]string, 0)
+	appendAnyDirs := func(path string) {
+		file, _ := os.Open(path)
+		fileInfo, _ := file.Stat()
+		if fileInfo.IsDir() {
+			fmt.Printf("Pushing %s directory\n", fileInfo.Name())
+			dirsToPush = append(dirsToPush, path)
+			return
+		}
+
+		fmt.Printf("File %s, size: %.2fKB, last modified: %s\n",
+			fileInfo.Name(), float64(fileInfo.Size())/1024.0,
+			fileInfo.ModTime().Format(time.ANSIC))
+	}
+
+	for {
+		if len(dirsToPush) == 0 {
+			appendAnyDirs(<-files)
+			continue
+		}
+
+		select {
+		case filepath := <-files:
+			appendAnyDirs(filepath)
+		case dirs <- dirsToPush[0]:
+			dirsToPush = dirsToPush[1:]
+		}
+	}
+}
